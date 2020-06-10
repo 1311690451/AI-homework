823c4159985d771a34395aa0fba0e69ee41b9feb
1311690451 <1311690451@qq.com>
2020-06-10 23:37:30 +0800

    调试之后忘记该回去了

--- 贝叶斯/Untitled.m
+++ 贝叶斯/Untitled.m
@@ -5,7 +5,7 @@ P1 = (Index(1) * Index(3)) / ((Index(1) - 1) * Index(3) + 1);
 P0 = (Index(2) * Index(3)) / ((Index(2) - 1) * Index(3) + 1);
 
 figure(1);
-aIndexis([0 1.1 0 1.1]);
+axis([0 1.1 0 1.1]);
 line([0 Index(4)] ,[P0 Index(3)],'color' , 'k' ,'linewidth' ,2);
 
 line([Index(4) 1] ,[Index(3) P1] ,'color' , 'k' ,'linewidth' ,2);
@@ -15,11 +15,11 @@ line([1 1] ,[0 P1] ,'linestyle' ,':');
 line([0 1] ,[P1 P1] ,'linestyle' ,':');
 
 title('主观BAYES');
-teIndext(0 ,P0 ,'P(H/~E)');
-teIndext(0 ,Index(3) ,'P(H)');
-teIndext(0 ,P1 ,'P(H/S)');
-teIndext(Index(4) ,0 ,'P(E)');
+text(0 ,P0 ,'P(H/~E)');
+text(0 ,Index(3) ,'P(H)');
+text(0 ,P1 ,'P(H/S)');
+text(Index(4) ,0 ,'P(E)');
 
-Indexlabel('P(E/S)');
+xlabel('P(E/S)');
 ylabel('P(H/S)');
 grid;
