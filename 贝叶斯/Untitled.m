84f82b7e4009af26bb1f236dfa14fd9bbeda1136
1311690451 <1311690451@qq.com>
2020-06-19 16:19:26 +0800

    小改一下

--- 贝叶斯/Untitled.m
+++ 贝叶斯/Untitled.m
@@ -6,13 +6,13 @@ P0 = (Index(2) * Index(3)) / ((Index(2) - 1) * Index(3) + 1);
 
 figure(1);
 axis([0 1.1 0 1.1]);
-line([0 Index(4)] ,[P0 Index(3)],'color' , 'k' ,'linewidth' ,2);
+line([0 Index(4)] ,[P0 Index(3)]);
 
-line([Index(4) 1] ,[Index(3) P1] ,'color' , 'k' ,'linewidth' ,2);
-line([Index(4) Index(4)] ,[0 Index(3)] ,'linestyle' ,':');
-line([0 Index(4)] ,[Index(3) Index(3)] ,'linestyle' ,':');
-line([1 1] ,[0 P1] ,'linestyle' ,':');
-line([0 1] ,[P1 P1] ,'linestyle' ,':');
+line([Index(4) 1] ,[Index(3) P1]);
+line([Index(4) Index(4)] ,[0 Index(3)]);
+line([0 Index(4)] ,[Index(3) Index(3)]);
+line([1 1] ,[0 P1]);
+line([0 1] ,[P1 P1]);
 
 title('主观BAYES');
 text(0 ,P0 ,'P(H/~E)');
